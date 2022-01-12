4110999928738b5013061597019ed5931e0b374b
abdukhamiddev <latipovabdukhamid@gmail.com>
2022-01-12 17:12:10 +0500

    changes

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,9 +1,9 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-	apiKey: process.env.API_KEY,
-	authDomain: process.env.AUTH_DOMAIN,
-	projectId: process.env.PROJECT_ID,
+	apiKey: "AIzaSyDi-Rm5JKIBDgOit1sxLLRB1KexpkeoR3Y",
+	authDomain: "my-unsplash-afddb.firebaseapp.com",
+	projectId: "my-unsplash-afddb",
 	storageBucket: "my-unsplash-afddb.appspot.com",
 	messagingSenderId: "438138722417",
 	appId: "1:438138722417:web:829d6590f1df32abb863d0",
